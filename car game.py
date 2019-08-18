5bdcff1e1e0923b1b97b3ce7b254951bc2e5771f
ck29140 <54242684+ck29140@users.noreply.github.com>
2019-08-18 20:59:17 +0530

    Add files via upload

---
+++ car game.py
@@ -0,0 +1,16 @@
+for i in range():
+    a=input(">")
+    if str(a)=="help":
+        print('''
+                 start-to start car
+                 stop-to stop car
+                 quit-to come out''')
+    elif str(a)=="start":
+        print("car started...ready to go")
+    elif str(a)=="stop":
+        print("car stopped")
+    elif str(a)=="quit":
+        break
+    else:
+        print("i dont understand")
+        
